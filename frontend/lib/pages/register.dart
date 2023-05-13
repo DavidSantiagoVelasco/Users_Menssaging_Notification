810b16884995f47e6a7218999df17a52aaae9a22
SantiagoVelasco <05santiagovelasco@gmail.com>
2023-05-13 12:31:42 -0500

    Image field in Register

--- frontend/lib/pages/register.dart
+++ frontend/lib/pages/register.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:frontend/imports.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -23,6 +25,7 @@ class _RegisterState extends State<Register> {
   final TextEditingController _surname = TextEditingController();
   final TextEditingController _number = TextEditingController();
   final TextEditingController _position = TextEditingController();
+  File? _image;
 
   void submit() async {
     if (!validate()) {
@@ -67,6 +70,56 @@ class _RegisterState extends State<Register> {
     return true;
   }
 
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedImage = await ImagePicker().pickImage(source: source);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+      });
+    }
+  }
+
+  void _showModal() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('ADD A PHOTO'),
+          content: const Text('Select photo from: '),
+          actions: [
+            TextButton(
+              onPressed: () {
+                _pickImage(ImageSource.camera);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Camera', style: TextStyle(color: Colors.blue)),
+            ),
+            TextButton(
+              onPressed: () async {
+                final ImagePicker picker = ImagePicker();
+                await picker
+                    .pickImage(
+                  source: ImageSource.gallery,
+                  maxWidth: 500,
+                  maxHeight: 500,
+                )
+                    .then((value) {
+                  if (value != null) {
+                    setState(() {
+                      _image = File(value.path);
+                    });
+                  }
+                  Navigator.of(context).pop();
+                });
+              },
+              child: const Text('Galery', style: TextStyle(color: Colors.blue)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -82,13 +135,86 @@ class _RegisterState extends State<Register> {
                   width: 350,
                 ),
               ),
-              CustomTextField(textEditingController: _name, text: "Name", password: false, number: false),
-              CustomTextField(textEditingController: _surname, text: "Surname", password: false, number: false),
-              CustomTextField(textEditingController: _number, text: "Number", password: false, number: true),
-              CustomTextField(textEditingController: _position, text: "Position", password: false, number: false),
-              CustomTextField(textEditingController: _email, text: "Email", password: false, number: false),
-              CustomTextField(textEditingController: _password, text: "Password", password: true, number: false),
-              CustomTextField(textEditingController: _password2, text: "Repeat password", password: true, number: false),
+              CustomTextField(
+                  textEditingController: _name,
+                  text: "Name",
+                  password: false,
+                  number: false),
+              CustomTextField(
+                  textEditingController: _surname,
+                  text: "Surname",
+                  password: false,
+                  number: false),
+              CustomTextField(
+                  textEditingController: _number,
+                  text: "Number",
+                  password: false,
+                  number: true),
+              CustomTextField(
+                  textEditingController: _position,
+                  text: "Position",
+                  password: false,
+                  number: false),
+              CustomTextField(
+                  textEditingController: _email,
+                  text: "Email",
+                  password: false,
+                  number: false),
+              CustomTextField(
+                  textEditingController: _password,
+                  text: "Password",
+                  password: true,
+                  number: false),
+              CustomTextField(
+                  textEditingController: _password2,
+                  text: "Repeat password",
+                  password: true,
+                  number: false),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(top: 10, bottom: 10, left: 28),
+                child: const Text("Your photo: ",
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+              ),
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 2,
+                  ),
+                  image: DecorationImage(
+                      fit: BoxFit.scaleDown,
+                      image: _image != null
+                          ? FileImage(_image!)
+                          : const NetworkImage(
+                                  "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg")
+                              as ImageProvider<Object>),
+                ),
+                child: Container(
+                  margin: const EdgeInsets.only(right: 60),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey,
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          _showModal();
+                        },
+                        icon: const Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
               Container(
                 margin: const EdgeInsets.only(top: 50),
                 child: SizedBox(
